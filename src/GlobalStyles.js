7f08f313ea0e8f49f8a22eb8a3274b861ba6c84b
DESKTOP-VVM0KFF\HTuan <tungxm113@gmail.com>
2022-03-16 01:36:58 +0700

    Add global styles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    :root {
+      --color-light: hsl(252, 30%, 95%);
+      --color-gray: hsl(253, 15%, 65%);
+      --color-primary: hsl(252, 75%, 60%);
+      --color-secondary: hsl(252, 100%, 90%);
+      --color-success: hsl(120, 95%, 65%);
+      --color-danger: hsl(0, 95%, 65%);
+      --color-dark: hsl(252, 30%, 17%);
+      --color-black: hsl(252, 30%, 10%);
+    
+      --border-radius: 2rem;
+      --card-border-radius: 1rem;
+      --btn-padding: 0.6rem;
+      --search-padding: 0.6rem 1rem;
+      --card-padding: 1rem;
+    }
+`;
+
+export default GlobalStyle;
